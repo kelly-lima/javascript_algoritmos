a9ce76d3ed809ec3dd73e27596ba9e96059b47dd
kelly cristina <kellycrisbarbosalima@gmail.com>
2022-09-16 23:09:44 -0300

    exercicios propostos no curso de JavaScript - Soulcode

---
+++ exercicio01/exercicio.js
@@ -0,0 +1,13 @@
+// Dado o tamanho da base e da altura de um retângulo, calcular a sua área 
+// e o seu perímetro.
+
+
+    const baseDoRetangulo = Number(prompt("Informe a base: "))
+    const alturaDoRetangulo = Number(prompt("Informe a altura: "))
+    const area = baseDoRetangulo * alturaDoRetangulo
+    const perimetro = 2*(baseDoRetangulo + alturaDoRetangulo)
+    alert("Área: " + area)
+    alert("Perimetro: " + perimetro)
+    
+    
+
---
+++ exercicio02/exercicio.js
@@ -0,0 +1,7 @@
+// Dado o tamanho do lado de um quadrado, calcular a área e o perímetro do mesmo.
+
+const lado = Number(prompt("Informe o lado do quadrado: "))
+const area = lado * lado
+const perimetro = lado * 4
+alert("Área: " + area)
+alert("Perimetro: " + perimetro)
